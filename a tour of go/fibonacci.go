8d4156ad63b1402052fbfd7ae4bd5e213808c63a
1121039 <max.blokker@leapforce.nl>
2022-05-10 11:52:06 +0200

    Almost finished fibonacci sequence

--- a tour of go/fibonacci.go
+++ a tour of go/fibonacci.go
@@ -2,13 +2,24 @@ package main
 
 import "fmt"
 
-func fibonacci() func() int {
+func fibonacci() func(int) int {
+	return func(i int) int {
+		var sequence int
+		var n1, n2 int = 0, 1
 
+		for x := 0; x <= i; x++ {
+			sequence = n1 + n2
+			n1 = n2
+			n2 = sequence
+		}
+
+		return sequence
+	}
 }
 
 func main() {
 	f := fibonacci()
 	for i := 0; i < 10; i++ {
-		fmt.Println(f())
+		fmt.Println(f(i))
 	}
 }
