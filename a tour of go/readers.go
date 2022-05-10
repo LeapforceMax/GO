6904d5dc524705a0b6146443a7c98ba40f0d6472
1121039 <max.blokker@leapforce.nl>
2022-05-10 16:48:20 +0200

    Add right exercise

--- a tour of go/readers.go
+++ a tour of go/readers.go
@@ -1,21 +1,11 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"strings"
-)
+import "golang.org/x/tour/reader"
+
+type MyReader struct{}
+
+// TODO: Add a Read([]byte) (int, error) method to MyReader.
 
 func main() {
-	r := strings.NewReader("Hello, Reader!")
-
-	b := make([]byte, 8)
-	for {
-		n, err := r.Read(b)
-		fmt.Printf("n = %v err = %v b = %v\n", n, err, b)
-		fmt.Printf("b[:n] = %q\n", b[:n])
-		if err == io.EOF {
-			break
-		}
-	}
+	reader.Validate(MyReader{})
 }
