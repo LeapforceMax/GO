92817f2e9182a069a2eaec0d3df240c2868f686a
1121039 <max.blokker@leapforce.nl>
2022-05-10 10:39:38 +0200

    Slices exercise

--- slices.go
+++ slices.go
@@ -1,12 +1,30 @@
 package main
 
-import "golang.org/x/tour/pic"
+import (
+	"image"
+
+	"golang.org/x/tour/pic"
+)
 
 func Pic(dx, dy int) [][]uint8 {
 
+	slice_y := make([][]uint8, dy)
 
-	
-	return [][]uint8
+	for y := 0; y < dy; y++ {
+		slice_x := make([]uint8, dx)
+
+		for x := 0; x < dx; x++ {
+			slice_x[x] = uint8((x + y) / 2)
+		}
+
+		slice_y[y] = slice_x
+	}
+
+	return slice_y
+}
+
+func ShowImage(m image.Image) {
+	ShowImage(m)
 }
 
 func main() {
